4289e0c3418598462c6d0b4db4ff17fe2cb386b5
Anton Mikheyeu <40431545+Anton000111@users.noreply.github.com>
2023-09-24 16:15:31 +0300

    T-5 Update text (#6)

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,7 @@
 const MANUAL = {
   KEYS: 'Use W, A, S, D keys for navigation',
   SECTIONS_USAGE: 'D - go into section, A - go back to previous section',
-  EXEC_COMMAND: 'Press Enter to run executable command\n',
+  EXEC_COMMAND: 'Press Enter to execute command\n',
 };
 
 const AFTER_EXECUTION = {
