270fb95136329d88ea481ba559ed0a67f675a5a8
Anton Mikheyeu <pro100633@gmail.com>
2025-10-04 23:03:03 +0200

    T7 Fix exporting issue

--- src/utils/colors.js
+++ src/utils/colors.js
@@ -1,7 +1,15 @@
-export const green = (text) => `\x1b[32m${text}\x1b[0m`;
+const green = (text) => `\x1b[32m${text}\x1b[0m`;
 
-export const red = (text) => `\x1b[31m${text}\x1b[0m`;
+const red = (text) => `\x1b[31m${text}\x1b[0m`;
 
-export const blue = (text) => `\x1b[34m${text}\x1b[0m`;
+const blue = (text) => `\x1b[34m${text}\x1b[0m`;
 
-export const yellow = (text) => `\x1b[33m${text}\x1b[0m`;
+const yellow = (text) => `\x1b[33m${text}\x1b[0m`;
+
+module.exports = {
+  green,
+  red,
+  blue,
+  yellow,
+};
+
